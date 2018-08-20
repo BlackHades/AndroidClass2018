ebc3eca06470ee32a7894ffc56739405cf9d273d
Doraemon <michealakinwonmi@gmail.com>
2018-08-20 17:49:54 +0100

    "Calculator Half Done"

--- app/src/main/java/fave/josh/com/androidclass/MainActivity.java
+++ app/src/main/java/fave/josh/com/androidclass/MainActivity.java
@@ -1,7 +1,11 @@
 package fave.josh.com.androidclass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = (Button) findViewById(R.id.start);
+
+        //When The Start button is clicked
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+
+                //Move to Next Activity
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
